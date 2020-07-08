97dccb746ca3afefe73f789c490e3b34b1301c48
evgao <evangao@126.com>
2020-07-08 05:47:48 +0000

    202007078

--- phase2/prometheus/k8s-custom-metrics/set_count.sh
+++ phase2/prometheus/k8s-custom-metrics/set_count.sh
@@ -4,5 +4,5 @@
 #
 counts=$1
 
-curl -X POST -H "Content-Type: application/json" 'http://10.110.113.114:4000/api/count' \
+curl -X POST -H "Content-Type: application/json" 'http://10.101.193.51:4000/api/count' \
 -d '{"count": '"$counts"'}'
